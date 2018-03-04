bb67140ff204bd2c16a13b4fd458fa6ceddbbf14
psharif <psharif@asu.edu>
2018-03-03 16:22:19 -0800

    Added Necessary Files For testing

---
+++ handlebar-routes.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+module.exports = function(app) {
+
+	app.get("/", function(req, res){
+		res.render("index");
+	});
+
+};
+
+
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
@@ -13,7 +14,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+//var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -24,14 +25,18 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Static directory
 app.use(express.static("public"));
 
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
+require("./routes/handlebar-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({}).then(function() {
+//db.sequelize.sync({}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+//});
