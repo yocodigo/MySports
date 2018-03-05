882bf9327db31cef7e31c7adc6f444882e2fa070
Fintech79 <abdelrahman.faisal@gmail.com>
2018-03-05 13:29:19 -0800

    latest u[pdates

--- config/api-scrapper.js
+++ config/api-scrapper.js
@@ -4,7 +4,7 @@ var twitterKeys = {
   consumer_key: 'dkwToGh1uUYl7GBrkrJu0n3MQ',
   consumer_secret: 'l4eao34oeb96tA63JZHYlDEoYcOLmLQ0VLDBYIdvR7nSEReUoh',
   access_token_key: '2807407982-N87cdI46rKU3GPDMzv5VC6sxmEuZxZzoN2OyXMW',
-  access_token_secret: 'dEhzJED4AYvCAnCccq0FkpsT8tBbea5MCeNR9mpJlq1fX',
+  access_token_secret: 'dEhzJED4AYvCAnCccq0FkpsT8tBbea5MCeNR9mpJlq1fX'
 }
 
 
@@ -16,11 +16,11 @@ twitterClient.get('statuses/user_timeline', twitterParams, function(error, tweet
       console.log(error);
     }else {
       var data = "";
-      tweets.forEach(function(el){
+      tweets.forEach(function(tweet){
         data += ""  
         +"====================================================="
-        + "\nTweeted at - " + el.created_at
-        + "\nTweet Text - " + el.text
+        + "\nTweeted at - " + tweet.created_at
+        + "\nTweet Text - " + tweet.text
         + "\n=====================================================\n";
       });
       console.log(data);
@@ -29,16 +29,23 @@ twitterClient.get('statuses/user_timeline', twitterParams, function(error, tweet
 });
 
 
-var probasketballapi = require("probasketballapi")
+//var Instagram = require("instagram")
 
-var probasketballapiKeys= {
 
-consumer_key: 'aUzFocEQCWLJBZ4N9RD5VqM31X8fbrlO',
+{
+    //"access_token": "fb2e77d.47a0479900504cb3ab4a1f626d174d2d",
+    //"user": {
+     //   "id": "1574083",
+     //   "username": "snoopdogg",
+     //   "full_name": "Snoop Dogg",
+     //   "profile_picture": "..."
+    }
+}
 
 }
 
-var probasketballClient = new ProBaskeball(proKeys);
-var proParams = { }
+//var probasketballClient = new ProBaskeball(proKeys);
+//var proParams = { }
 
 //<?php
 
@@ -59,6 +66,33 @@ var proParams = { }
 
 //curl_close($ch);
 
-//echo $result;
+//echo $result;it 
 
 //?>
+
+
+
+
+
+var Stubhub = require("stubhub");
+
+var stubhubKeys {
+
+Application Token: '64a2a266-6c7a-3092-ab63-08453aa9fa6b',
+Consumer Key: '1sofM52NIkKWQNxYLeneFoezTaYa',
+Consumer Secret: 'DOWD8VCBXJNMfPf5NpUrNMbXcKka'
+}
+
+var stubhubKeys = function(){
+
+  var Client = new Stubhub (stubhubKeys)
+  var stubhubParams = ( user_name: "" , events: 10)
+};
+
+stubhubKeys.get( 'user.event', stubhubParams, error response ) 
+if error{
+  console.log(error);
+};
+else { var data: "";
+
+}
