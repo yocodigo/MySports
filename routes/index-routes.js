b6dcf49293b63930e6b40506142fab375dcb6ee0
psharif <psharif@asu.edu>
2018-03-07 19:29:14 -0800

    Added Changes

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,8 +1,39 @@
 var router = require('express').Router(); 
 var db = require("../models");
 
+  //MiddleWare to check if the user is logged in or not.
+var authCheck = function(req, res, next){
+	if(!req.user){
+		//If the user is not logged In 
+		res.redirect('/auth/login');
+	}
+	else{
+		next();
+	}
+}
 
-  router.get("/", function(req, res) {
-      res.render("index", {user: req.user});
-    });
-    module.exports = router; 
+
+router.get("/", function(req, res) {
+   res.redirect('/auth/login');
+  //res.render("index", {user: req.user});
+});
+
+
+router.get("/signin", authCheck, function(req, res){
+	db.Fan.findOne({
+			where: {
+				googleID: req.user.googleID
+			}
+		}).then(function(dbFan){
+			//If the Fan is in the database
+			if(dbFan){
+				res.render("index", dbFan.dataValues);
+			}
+			else{
+				//otherwise they must signup
+				return res.redirect('/auth/signup');
+			}
+	});
+});
+
+ module.exports = router; 
