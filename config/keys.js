33ba17239150570be76aced252e0f6066d67a9b7
psharif <psharif@asu.edu>
2018-03-08 22:07:33 -0800

    Added Keys.js

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+   google: {
+       clientID: '63268386602-323u3790iru3qcm21he3u3rgl1dlu26h.apps.googleusercontent.com',
+       clientSecret: 'FxU_RGJe_2z1JAqKh2kDDvM1'
+   },
+   cookieSession :{
+       cookieKey: 'thecookiemonstersayscisforcookie'
+   },
+
+twitterKeys : {
+       consumer_key: 'dkwToGh1uUYl7GBrkrJu0n3MQ',
+      consumer_secret: 'l4eao34oeb96tA63JZHYlDEoYcOLmLQ0VLDBYIdvR7nSEReUoh',
+      access_token_key: '2807407982-N87cdI46rKU3GPDMzv5VC6sxmEuZxZzoN2OyXMW',
+      access_token_secret: 'dEhzJED4AYvCAnCccq0FkpsT8tBbea5MCeNR9mpJlq1fX'
+   }
+
+};
